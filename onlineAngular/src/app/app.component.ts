e1765df1dfef1cec12d957c4ffdc097dcb7cb78e
diksharana <sharma.diksharana@gmail.com>
2020-06-10 16:40:02 +0530

    ngfor

--- onlineAngular/src/app/app.component.ts
+++ onlineAngular/src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   // book
   selector: 'app-start',
-  template: '<h1>Hello Angular..!</h1>', // view : UI
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  //template: '<h1>Hello Angular..!</h1>', // view : UI
 })
 export class AppComponent {
   // data and logic
+  title = ' Welcome to Angular..!';
+
+  arr = ['himachal', 'punjab', 'mumbai'];
+
+  mobParts = [
+    {
+      id: 1001,
+      name: 'Screen 5.5',
+      description: '5.5 Screen for Moto g',
+      inStock: 5,
+    },
+    {
+      id: 1002,
+      name: 'Screen 5',
+      description: '5 Screen for Samsung',
+      inStock: 8,
+    },
+    {
+      id: 1003,
+      name: 'key panel',
+      description: 'Key panel for Nokia',
+      inStock: 0,
+    },
+  ];
 }
