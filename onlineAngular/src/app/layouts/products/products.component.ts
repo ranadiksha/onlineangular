7331b77f977647ea4c1c28f80e7cdefb0cd0b739
diksharana <sharma.diksharana@gmail.com>
2020-07-05 18:00:45 +0530

    outside products

--- onlineAngular/src/app/layouts/products/products.component.ts
+++ onlineAngular/src/app/layouts/products/products.component.ts
@@ -10,39 +10,4 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {}
   title = ' Welcome to Angular..!';
-
-  arr = ['himachal', 'punjab', 'mumbai'];
-
-  mobParts = [
-    {
-      id: 1001,
-      name: 'Screen 5.5',
-      description: '5.5 Screen for Moto g',
-      inStock: 5,
-      prodColor: 'brown',
-      canPurch: true,
-      country: 'Australia',
-      price: 1200,
-    },
-    {
-      id: 1002,
-      name: 'Screen 5',
-      description: '5 Screen for Samsung',
-      inStock: 8,
-      prodColor: 'green',
-      canPurch: false,
-      country: 'India',
-      price: 1200,
-    },
-    {
-      id: 1003,
-      name: 'key panel',
-      description: 'Key panel for Nokia',
-      inStock: 0,
-      prodColor: 'red',
-      canPurch: false,
-      country: 'Germany',
-      price: 1200,
-    },
-  ];
 }
--- onlineAngular/src/app/product/mob-parts/mob-parts.component.ts
+++ onlineAngular/src/app/product/mob-parts/mob-parts.component.ts
@@ -3,14 +3,52 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mob-parts',
   templateUrl: './mob-parts.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class MobPartsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  arr = ['himachal', 'punjab', 'mumbai'];
 
-  ngOnInit(): void {
+  mobParts = [
+    {
+      id: 1001,
+      name: 'Screen 5.5',
+      description: '5.5 Screen for Moto g',
+      inStock: 5,
+      prodColor: 'brown',
+      canPurch: true,
+      country: 'Australia',
+      price: 1200,
+    },
+    {
+      id: 1002,
+      name: 'Screen 5',
+      description: '5 Screen for Samsung',
+      inStock: 8,
+      prodColor: 'green',
+      canPurch: false,
+      country: 'India',
+      price: 1200,
+    },
+    {
+      id: 1003,
+      name: 'key panel',
+      description: 'Key panel for Nokia',
+      inStock: 0,
+      prodColor: 'red',
+      canPurch: false,
+      country: 'Germany',
+      price: 1200,
+    },
+  ];
+
+  calcProd() {
+    let tot = 0;
+    for (let mobPart of this.mobParts) {
+      tot = tot + mobPart.inStock;
+    }
+    return tot;
   }
-
 }
